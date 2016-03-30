5b68ad67769dac06d39c9671df4b007650c3834a
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-30 09:31:52 -0400

    simple contacts component created

---
+++ src/js/contacts.js
@@ -0,0 +1,16 @@
+
+
+
+
+<div class="singleView">
+  		<div class="pictureBlock">
+  			<img src="">
+  		</div>
+  		<ul>
+  			<li>icon<span>Name</span></li>
+  			<li>icon<span>Email</span></li>
+  			<li>icon<span>phone</span></li>
+  			<li>icon<span>cityState</span></li>
+  		</ul>
+  	</div>
+  </div>
