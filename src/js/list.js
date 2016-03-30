5873da8301cac775bd40bbb354e6db8b170e735a
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-30 09:16:44 -0400

    simple component created
    for List of "friends"

---
+++ src/js/list.js
@@ -0,0 +1,15 @@
+    <div class="peeps">
+  		<header>My Cronies</header>
+  		<div class="buddy1">
+  			<img src="" >"Name" />
+  		</div>
+  		<div class="buddy2">
+  			<img src="" >"Name" />
+  		</div>
+  		<div class="buddy3">
+  			<img src="" >"Name" />
+  		</div>
+  		<div class="buddy4">
+  			<img src="" >"Name" />
+  		</div>
+    </div>
